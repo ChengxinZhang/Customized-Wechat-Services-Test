7950f3ba81ed3bbb035c7b0037b3abd3c891ab93
Chengxin <16414132+ChengxinZhang@users.noreply.github.com>
2020-03-19 22:18:42 +0800

    增加注释

--- tuling.py
+++ tuling.py
@@ -4,7 +4,8 @@ import json
 def talk(content,userId):
     #reqType = 0
     url = 'http://www.tuling123.com/openapi/api'
-    apiKey = 'b086a72ed3eb4f9794a1a84e817a438c'
+    apiKey = 'tuling账户的apikey'
+#构建请求参数
     data = {      
         'key':apiKey,
         'info':content, 
@@ -13,6 +14,7 @@ def talk(content,userId):
     }
     json_data = json.dumps(data)
     req = requests.post(url,data = json_data)
+#将返回值转为json格式去提取
     result = json.loads(req.text)
     if result['code'] == 200000:
         return result['text']+result['url']
